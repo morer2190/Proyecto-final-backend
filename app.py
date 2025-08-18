3efa94eb8e8d655fa4901b49a3bf7e39fbf989c1
morera2190 <fernanda.morera.cortes@gmail.com>
2025-08-17 21:03:56 -0600

    Cambios para validacion de JWT + Hash

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import os
+import bcrypt
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String, Enum as SqlEnum, Date
 from dotenv import load_dotenv
 from enum import Enum
+from decorator.role_required import role_required
 load_dotenv()
 
 app = Flask(__name__)
@@ -15,11 +18,15 @@ db_password = os.getenv('DB_PASSWORD', '')
 app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+mysqlconnector://{db_user}:{db_password}@localhost/default?auth_plugin=caching_sha2_password'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY")
+jwt = JWTManager(app)
+
 db = SQLAlchemy(app)
 
 class Rol(Enum):
     Cliente = 1
     Agente = 2
+    Administrador = 3
 
 class EstadoCotizacion(Enum):
     Pendiente = 1
@@ -43,11 +50,23 @@ class Usuario(db.Model):
     nombre = Column(String(200))
     cedula = Column(String(50), unique=True)
     correo_electronico = Column(String(100))
-    contrasena = Column(String(100))
+    hash_contrasena = Column(String(100))
     rol = Column(SqlEnum(Rol), default=Rol.Cliente)
 
+     # Método para setear la contraseña
+    def set_password(self, password: str):
+        salt = bcrypt.gensalt()
+        self.hash_contrasena = bcrypt.hashpw(password.encode("utf-8"), salt).decode("utf-8")
+    
+    # Método para verificar la contraseña
+    def check_password(self, password: str) -> bool:
+        return bcrypt.checkpw(password.encode("utf-8"), self.hash_contrasena.encode("utf-8"))
+
     def to_dict(self):
-        return {"id": self.id, "nombre": self.nombre, "cedula": self.cedula, "correo_electronico": self.correo_electronico, "rol": Rol(self.rol).name }
+        return {"id": self.id, "nombre": self.nombre, "cedula": self.cedula, "correo_electronico": self.correo_electronico, "rol": self.get_rol_name() }
+    
+    def get_rol_name(self):
+        return self.rol.name if self.rol else None
 
 class Proveedor(db.Model):
     id = Column(Integer, primary_key=True, autoincrement=True)
@@ -92,8 +111,33 @@ class Reservacion(db.Model):
 def inicio():
     return ""
 
+# Login que genera un token
+@app.route("/login", methods=["POST"])
+def login():
+
+    data = request.get_json()
+
+    if not data.get('correo_electronico'):
+        return jsonify({"error": "El campo 'correo_electronico' es obligatorio"}), 400
+
+    if not data.get('contrasena'):
+        return jsonify({"error": "El campo 'contrasena' es obligatorio"}), 400
+
+    correo_electronico = data.get("correo_electronico", None)
+    contrasena = data.get("contrasena", None)
+
+    usuarios = Usuario.query.filter_by(correo_electronico=correo_electronico).all()
+
+    if not usuarios or not usuarios[0].check_password(contrasena):
+        return jsonify({"msg": "Credenciales inválidas"}), 401
+
+    token = create_access_token(identity=correo_electronico, additional_claims={'role': usuarios[0].get_rol_name()})
+    return jsonify(access_token=token)
+
+
 # Ruta GET para obtener todos los usuarios
 @app.route('/usuarios', methods=['GET'])
+@role_required([Rol.Administrador.name])  # Solo permite acceso a usuarios con rol Administrador
 def obtener_usuarios():
     usuarios = Usuario.query.all()
     return jsonify([u.to_dict() for u in usuarios])
@@ -107,29 +151,38 @@ def crear_usuario():
         return jsonify({"error": "El campo 'nombre' es obligatorio"}), 400
     if not data.get('cedula'):
         return jsonify({"error": "El campo 'cedula' es obligatorio"}), 400
+    if not data.get('contrasena'):
+        return jsonify({"error": "El campo 'contrasena' es obligatorio"}), 400
+    if not data.get('correo_electronico'):
+        return jsonify({"error": "El campo 'correo_electronico' es obligatorio"}), 400
     rol_value = data['rol']
     if isinstance(rol_value, int):
         rol_enum = Rol(rol_value)
     else:
         rol_enum = Rol[rol_value]
+  
     nuevo_usuario = Usuario(
         nombre=data['nombre'],
         cedula=data['cedula'],
         correo_electronico=data['correo_electronico'],
-        contrasena=data['contrasena'],
         rol=rol_enum
     )
+    nuevo_usuario.set_password(data.get('contrasena'))
+
+
     db.session.add(nuevo_usuario)
     db.session.commit()
     return jsonify({"mensaje": "Usuario creado", "usuario": nuevo_usuario.to_dict()}), 201
 
 # --- CRUD Proveedor ---
 @app.route('/proveedores', methods=['GET'])
+@role_required([Rol.Administrador.name])  # Solo permite acceso a usuarios con rol Administrador
 def obtener_proveedores():
     proveedores = Proveedor.query.all()
     return jsonify([p.to_dict() for p in proveedores])
 
 @app.route('/proveedores', methods=['POST'])
+@role_required([Rol.Administrador.name, Rol.Agente.name])
 def crear_proveedor():
     data = request.get_json()
     # Validación
@@ -143,6 +196,7 @@ def crear_proveedor():
     return jsonify({"mensaje": "Proveedor creado", "proveedor": nuevo.to_dict()}), 201
 
 @app.route('/proveedores/<int:id>', methods=['PUT'])
+@role_required([Rol.Administrador.name, Rol.Agente.name])
 def actualizar_proveedor(id):
     data = request.get_json()
     proveedor = Proveedor.query.get_or_404(id)
@@ -155,6 +209,7 @@ def actualizar_proveedor(id):
     return jsonify(proveedor.to_dict())
 
 @app.route('/proveedores/<int:id>', methods=['DELETE'])
+@role_required([Rol.Administrador.name])
 def eliminar_proveedor(id):
     proveedor = Proveedor.query.get_or_404(id)
     db.session.delete(proveedor)
@@ -163,11 +218,13 @@ def eliminar_proveedor(id):
 
 # --- CRUD Cotizacion ---
 @app.route('/cotizaciones', methods=['GET'])
+@role_required([Rol.Administrador.name, Rol.Agente.name])
 def obtener_cotizaciones():
     cotizaciones = Cotizacion.query.all()
     return jsonify([c.to_dict() for c in cotizaciones])
 
 @app.route('/cotizaciones', methods=['POST'])
+@role_required([Rol.Administrador.name, Rol.Agente.name])
 def crear_cotizacion():
     data = request.get_json()
     estado = data['estado']
@@ -178,6 +235,7 @@ def crear_cotizacion():
     return jsonify({"mensaje": "Cotización creada", "cotizacion": nueva.to_dict()}), 201
 
 @app.route('/cotizaciones/<int:id>', methods=['PUT'])
+@role_required([Rol.Administrador.name, Rol.Agente.name])
 def actualizar_cotizacion(id):
     data = request.get_json()
     cotizacion = Cotizacion.query.get_or_404(id)
@@ -190,6 +248,7 @@ def actualizar_cotizacion(id):
     return jsonify(cotizacion.to_dict())
 
 @app.route('/cotizaciones/<int:id>', methods=['DELETE'])
+@role_required([Rol.Administrador.name, Rol.Agente.name])
 def eliminar_cotizacion(id):
     cotizacion = Cotizacion.query.get_or_404(id)
     db.session.delete(cotizacion)
@@ -198,11 +257,13 @@ def eliminar_cotizacion(id):
 
 # --- CRUD Reservacion ---
 @app.route('/reservaciones', methods=['GET'])
+@role_required([Rol.Administrador.name, Rol.Agente.name])
 def obtener_reservaciones():
     reservaciones = Reservacion.query.all()
     return jsonify([r.to_dict() for r in reservaciones])
 
 @app.route('/reservaciones', methods=['POST'])
+@role_required([Rol.Administrador.name, Rol.Agente.name])
 def crear_reservacion():
     data = request.get_json()
     estado = data.get('estado', EstadoReservacion.Confirmada.name)
@@ -219,6 +280,7 @@ def crear_reservacion():
     return jsonify({"mensaje": "Reservación creada", "reservacion": nueva.to_dict()}), 201
 
 @app.route('/reservaciones/<int:id>', methods=['PUT'])
+@role_required([Rol.Administrador.name, Rol.Agente.name])
 def actualizar_reservacion(id):
     data = request.get_json()
     reservacion = Reservacion.query.get_or_404(id)
@@ -234,6 +296,7 @@ def actualizar_reservacion(id):
     return jsonify(reservacion.to_dict())
 
 @app.route('/reservaciones/<int:id>', methods=['DELETE'])
+@role_required([Rol.Administrador.name, Rol.Agente.name])
 def eliminar_reservacion(id):
     reservacion = Reservacion.query.get_or_404(id)
     db.session.delete(reservacion)
